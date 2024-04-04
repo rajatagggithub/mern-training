ddbbc9e54675394acdc60baff2b952e6ce8c5092
rajatagggithub <103359164+rajatagggithub@users.noreply.github.com>
2024-04-04 20:05:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// var message = "hello how r uh";
+// console.log(message);
+// function fun(){
+//     for(var i=0;i<5; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+
+// // for(var i=0; i<5; i++){
+// //     console.log(i);
+// // }
+// fun();
+//     let message="else";
+//     console.msg(message);
+// let arr=['a','b','c','d'];
+// arr[0] = 'abcd';
+// console.log(arr);
+
+// let str = "12";
+// let age=20;
+
+
+// let ans= str+age;
+// let ans1= str-age;
+// console.log(ans);
+// console.log(ans1);
+
+let a ='12';
+let b = 12;
+if(a==b){
+    console.log("yes");
+
+}
+else{
+    console.log("no");
+}
+
+if(a===b){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
+const str3 = 'Rajat';
+const str4 =" my name is rajat";  
+// const ans3 = `${str3} ${str4}`; 
+const ans3 = str3 + str4;
+console.log(ans3); 
+
+const c= 'looking';
+const d= 'like';
+const e= 'wow';
+const ans4 = `${c} ${d} ${e}`;
+console.log(ans4);
+
+function print(str){
+console.log(str);
+}
+
+const s = 'hello';
+print ('hello');
+
+function sum(a=0, b=0){
+    if(a && b){
+    console.log(a+b);}
+}
+
+sum(20);
+
+let f = 1;
+if(a){
+    console.log('yes');
+    }
+    else{
+        console.log('no');
+    }
+
+    function print(){
+        console.log('--');
+    }
+    let g = 2;
+
+    switch(g){
+        case 1:
+            console.log('one');
+            break;
+            case 2:
+                console.log('two');
+                break;
+        
+                case 3:
+                console.log('three');
+                break;
+    }
+
+    const obj={
+        1:'one',
+        'name': 'anshika',
+    }
+    console.log(obj);
+
+    let h = prompt('enter your name');
+    console.log(h);
+
+
+
+
+
+
+
