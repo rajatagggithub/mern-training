935f64420352337ee8b4b38c453b8923f0cc542e
rajatagggithub <103359164+rajatagggithub@users.noreply.github.com>
2024-05-29 22:10:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+// let a = new string ("hello");
+// let b = "hello";
+
+// const obj = {
+//     "name": "ajay",
+//     "last name": "aggarwal"
+
+// }
+// obj.name = 20;
+// console.log(obj);
+
+// let arr = [];
+// console.log(arr);
+
+// const arr = [1,2,34];
+// arr[10] = 20;
+// console.log(arr);
+
+// const arr = [1,2,34];
+// arr.push(10) = 20;
+// console.log(arr);
+
+// const arr = [1,2,34];
+// console.log(typeof (arr));
+
+// const obj = {
+//       "name": "ajay",
+//         "last name": "aggarwal"}
+//         const arr = [1,2,34];
+//         arr[2] = 4;
+//         console.log(typeof (obj));
+//  console.log(typeof(arr));
+
+// const arr = [1,2,34];
+
+// console.log(array.isArray(obj))
+// onsole.log(array.isArray(arr))
+
+// checkIfObject([1,2,3,4])
+
+// checkIfObject({"name" :"ajay"})
+
+// function checkIfObject(x){
+//     if(Array.isArray(x)){
+//         console.log("Not object");
+//     }
+//     else if(typeof(x) =="object"){
+//         console.log('object');
+//     }
+//     else{
+//         console.log('not object')
+//     }
+// }
+
+// const arr = ['name', 2]
+// const obj = {
+//     'name':"ajay", age:20, 1:"else"
+// };
+// for(let i of arr){
+//     console.log(i);
+// }
+// for(let i of obj){
+//     console.log(i);
+// }
+
+// for(let i in arr){
+//         console.log(i);
+//      }
+//      for(let i in obj){
+//         console.log(i);
+//      }
+
+// dom
+
+// console.log(window)
+// console.log(window.innerheight)
+// console.log(window.innerwidth)
+
+
+// console.log(window.document)
+
+// console.dir(window.document)
+
+// const res = document.getElementsByTagName('h3')
+
+// console.log(res)
+
+const res = document.getElementById('ht1')
+res[innerhtml] = "hello, world!";
+console.log(res)
+
+// const res = document.querySelectorAll('h3')
+// console.log(res);
+
+const ne =  document.createElement("h3")
+// console.log(ne)
+ne.innertext = "Dynamic text"
+// console.log(ne);
+firstDiv[0].appendChild(ne);
+
+
+
+
